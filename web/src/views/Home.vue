24077a1723e6a2ac06a08e9540dc3e89b674bfb8
NEDONION <nedjiachenghu@gmail.com>
2022-02-03 23:30:22 -0600

    5-4 封面小图样式调整

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -49,10 +49,27 @@
         <a-layout-content
           :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-          Content
-          <pre>
-          {{ebooks}}
-          </pre>
+          <a-list 
+          item-layout="vertical" size="large" 
+          :grid="{ gutter: 20, column: 3 }"
+          :data-source="ebooks">
+    <template #renderItem="{ item }">
+      <a-list-item key="item.name">
+        <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+        </template>
+        <a-list-item-meta :description="item.description">
+          <template #title>
+            <a :href="item.href">{{ item.name }}</a>
+          </template>
+          <template #avatar><a-avatar :src="item.cover" /></template>
+        </a-list-item-meta>
+      </a-list-item>
+    </template>
+  </a-list>
         </a-layout-content>
       </a-layout>
     </a-layout>
@@ -61,6 +78,20 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref } from 'vue';
 import axios from "axios";
+
+const listData: any = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
 
 export default defineComponent({
   name: 'Home',
@@ -79,28 +110,35 @@ export default defineComponent({
     });
   });
 
+  // // 分页组件
+  //   const pagination = {
+  //     onChange: (page: number) => {
+  //       console.log(page);
+  //     },
+  //     pageSize: 3,
+  //   };
+
+    const actions: Record<string, string>[] = [
+      { type: 'StarOutlined', text: '156' },
+      { type: 'LikeOutlined', text: '156' },
+      { type: 'MessageOutlined', text: '2' },
+    ];
+
     return {
-      ebooks
+      ebooks,
+      listData,
+      actions
     }
 
 }});
 </script>
 
-<style>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
-
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-}
+<style scoped>
+  .ant-avatar {
+    width: 50px;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 8%;
+    margin: 5px 0;
+  }
 </style>
