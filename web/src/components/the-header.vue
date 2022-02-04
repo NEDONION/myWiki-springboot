81bc00ea06ee54c978118426de143c9cc900f978
NEDONION <nedjiachenghu@gmail.com>
2022-02-04 15:39:27 -0600

    6-2 add 电子书管理页面

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -7,9 +7,15 @@
         v-model:selectedKeys="selectedKeys1"
         :style="{ lineHeight: '64px' }"
       >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
+        <a-menu-item key="/">
+          <router-link to="/">Homapage</router-link>
+          </a-menu-item>
+        <a-menu-item key="/admin/ebook">
+          <router-link to="/admin/ebook">Ebook Management</router-link>
+          </a-menu-item>
+        <a-menu-item key="/about">
+          <router-link to="/about">About Us</router-link>
+          </a-menu-item>
       </a-menu>
     </a-layout-header>
 </template>
